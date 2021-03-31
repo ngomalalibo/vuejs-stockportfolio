87e3d78f58bfaa7b294250d383478d032f5893d9
Ngo <ngomalalibo@yahoo.com>
2021-03-31 11:04:52 +0100

    Initial Commit

--- src/data/stocks.js
+++ src/data/stocks.js
@@ -2,5 +2,13 @@ export default [
     {id: 1, name: 'BMW', price: 110},
     {id: 2, name: 'Google', price: 200},
     {id: 3, name: 'Apple', price: 250},
-    {id: 4, name: 'Twitter', price: 8}
+    {id: 4, name: 'Twitter', price: 8},
+    {id: 5, name: 'Microsoft', price: 110},
+    {id: 6, name: 'Tesla', price: 200},
+    {id: 7, name: 'Zoom', price: 250},
+    {id: 8, name: 'Facebook', price: 8},
+    {id: 9, name: 'SalesForce', price: 110},
+    {id: 10, name: 'Cisco', price: 200},
+    {id: 11, name: 'PayPal', price: 250},
+    {id: 12, name: 'Intel', price: 8}
 ];
